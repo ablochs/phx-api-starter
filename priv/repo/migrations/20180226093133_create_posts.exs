f715ae34c3fe899d83efc2401f9845a8a824af1d
ablochs <ablochs@gmail.com>
2018-02-26 10:38:35 +0100

    Update posts constaints

--- priv/repo/migrations/20180226093133_create_posts.exs
+++ priv/repo/migrations/20180226093133_create_posts.exs
@@ -3,8 +3,8 @@ defmodule Blog.Repo.Migrations.CreatePosts do
 
   def change do
     create table(:posts) do
-      add :name, :string
-      add :user_id, references(:users, on_delete: :nothing)
+      add :name, :string, null: false
+      add :user_id, references(:users, on_delete: :delete_all)
 
       timestamps()
     end
