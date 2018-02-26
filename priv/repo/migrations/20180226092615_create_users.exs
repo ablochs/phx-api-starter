422071f4500f089c306dc661678829872ef31883
ablochs <ablochs@gmail.com>
2018-02-26 10:30:11 +0100

    Update constraint on user migration

--- priv/repo/migrations/20180226092615_create_users.exs
+++ priv/repo/migrations/20180226092615_create_users.exs
@@ -3,15 +3,17 @@ defmodule Blog.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :email, :string
+      add :email, :string, null: false
       add :first_name, :string
       add :last_name, :string
-      add :password_hash, :string
+      add :password_hash, :string, null: false
       add :is_admin, :boolean, default: false, null: false
       add :email_verified, :boolean, default: false, null: false
 
       timestamps()
     end
+
+    create unique_index(:users, [:email])
 
   end
 end
